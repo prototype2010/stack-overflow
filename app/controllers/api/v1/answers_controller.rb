d49f36d7867761f87a758d129f96ebeb645fcb15
prototype2010 <alex.zakorko2015@gmail.com>
2023-09-15 21:58:11 +0300

    questions answer

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AnswersController < Api::V1::BaseController
   def index
-    render json: Question.all
+    render json: Question.find(params[:question_id])&.answers
   end
 end
---
+++ spec/api/v1/answers_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe 'Question answers API', type: :request do
+  let(:headers) {
+    { "CONTENT_TYPE": 'application/json' ,
+      "ACCEPT": 'application/json'  }
+  }
+  let(:question) { create(:question, :with_answers)}
+
+  let(:api_path) { api_v1_question_answers_path(question) }
+
+
+  describe 'GET /api/v1/questions/:id/answers' do
+
+    context 'unauthorized' do
+      it_behaves_like 'API Authorizable' do
+        let(:method) { :get }
+        let(:params) { {id: question.id} }
+      end
+    end
+
+    context 'authorized' do
+      let(:user) { create(:user) }
+      let(:access_token) { create(:access_token, resource_owner_id: user.id).token}
+
+      before do
+        get api_v1_question_answers_path(question), params: {access_token: access_token}, headers: headers
+      end
+
+      context 'Answers' do
+        it 'returns array same size' do
+          expect(json['answers'].size).to eq question.answers.size
+        end
+
+        it 'returns all public fields' do
+          %w[id body created_at updated_at comments].each do |attr|
+            expect(json['answers'].first[attr])
+              .to eq question.answers.first.public_send(attr).as_json
+          end
+        end
+      end
+    end
+  end
+end
+
