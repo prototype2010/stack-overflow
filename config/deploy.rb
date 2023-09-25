e6375eb98a159660b952180020d64e8c4c232dfc
prototype2010 <alex.zakorko2015@gmail.com>
2023-09-25 20:04:19 +0300

    deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ end
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key', 'config/credentials.yml.enc', 'config/.env'
+append :linked_files, "config/database.yml", 'config/master.key', 'config/.env'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
