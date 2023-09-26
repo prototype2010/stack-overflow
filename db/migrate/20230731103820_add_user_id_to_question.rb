0d4203825ba2b90939ee2ee3f4a4e22ae4355178
prototype2010 <alex.zakorko2015@gmail.com>
2023-09-26 14:24:55 +0300

    deploy config

--- db/migrate/20230731103820_add_user_id_to_question.rb
+++ db/migrate/20230731103820_add_user_id_to_question.rb
@@ -1,5 +1,5 @@
 class AddUserIdToQuestion < ActiveRecord::Migration[7.0]
   def change
-    add_reference :questions, :author, foreign_key: { to_table: :users }, null: false, default: User.first.id
+    add_reference :questions, :author, foreign_key: { to_table: :users }, null: false
   end
 end
